bc13cc22b1ae26560010660f00a9f6d9f65727a8
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-01-11 11:57:21 -0300

    fix: removing moduleFileExtensions from jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  moduleFileExtensions: ['ts', 'js'],
   modulePathIgnorePatterns: ['tests', 'dist', 'config']
 }
