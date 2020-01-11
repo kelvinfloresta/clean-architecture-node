f06611078f3c128d6777950a54dca95b72b5b76a
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-01-11 01:12:18 -0300

    fix: test should call start in startServer

--- src/start.unit.test.ts
+++ src/start.unit.test.ts
@@ -3,7 +3,7 @@ import startServer from './start'
 describe('startServer method', () => {
   const ANY_PORT = 0
   it('should call start', async () => {
-    const server: any = { start: Promise.resolve() }
+    const server: any = { start: async () => Promise.resolve() }
     const spy = jest.spyOn(server, 'start')
     await startServer(server, ANY_PORT)
     expect(spy).toBeCalledWith(ANY_PORT)
