9997e4b15af1e7f88251103b421bdab40062a968
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2020-01-10 23:47:43 -0300

    feat: add startServer test to ensure that the start method is called

---
+++ src/start.unit.test.ts
@@ -0,0 +1,11 @@
+import startServer from './start'
+
+describe('startServer method', () => {
+  const ANY_PORT = 0
+  it('should call start', async () => {
+    const server: any = { start: Promise.resolve() }
+    const spy = jest.spyOn(server, 'start')
+    await startServer(server, ANY_PORT)
+    expect(spy).toBeCalledWith(ANY_PORT)
+  })
+})
