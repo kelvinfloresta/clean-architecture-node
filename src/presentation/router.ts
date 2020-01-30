5b39a504c0aa03a6f23a72188d282b4c5e5f04d0
Kelvin Floresta de Andrade <kelvin2503@zicard-accentiv.com.br>
2020-01-30 15:40:52 -0300

    rollback

--- src/presentation/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Protocol, Service } from 'restana'
-
-export function routerV1<T extends Protocol> (app: Service<T>): Router<T> {
-  const route = app.newRouter()
-  route.get('/', (_, res) => {
-    res.send('Hello world!')
-  })
-  return route
-}
